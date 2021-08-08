f66cb5c5a2c91d0ef4efd84bf4779b1df174c624
lanndrich <lanndrich@gmail.com>
2021-08-07 23:20:02 -0700

    LoadMore button functionality and style

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,6 +12,7 @@ import Grid from './Grid';
 import Thumb from './Thumb';
 import Spinner from './Spinner';
 import SearchBar from './SearchBar';
+import Button from './Button';
 
 // Hook
 import { useHomeFetch } from '../hooks/useHomeFetch';
@@ -20,9 +21,11 @@ import { useHomeFetch } from '../hooks/useHomeFetch';
 import NoImage from '../images/no_image.jpg';
 
 const Home = () => {
-	const { state, loading, error, searchTerm, setSearchTerm } = useHomeFetch();
+	const { state, loading, error, searchTerm, setSearchTerm, setIsLoadingMore } = useHomeFetch();
 
 	console.log(state);
+
+	if(error) return <div>Something went wrong.  Please evacuate immediately.</div>
 
 	return (
 		<Fragment>
@@ -49,7 +52,10 @@ const Home = () => {
 					/>
 				))}
 			</Grid>
-			<Spinner />
+			{loading && <Spinner />}
+			{state.page < state.total_pages && !loading && (
+				<Button text='Load More' callback={() => setIsLoadingMore(true)} />
+			)}
 		</Fragment>
 	);
 }
